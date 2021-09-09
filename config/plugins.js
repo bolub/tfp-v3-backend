9a97e5355ca3fc19b95e35dca8636f46be60d416
bolub <abiol5202@gmail.com>
2021-09-09 08:18:25 +0100

    fix env issue with sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -22,7 +22,7 @@ module.exports = ({ env }) => ({
   email: {
     provider: "sendgrid",
     providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
+      apiKey: process.env.SENDGRID_API_KEY,
     },
     settings: {
       defaultFrom: "Tolumitfpuk@gmail.com",
